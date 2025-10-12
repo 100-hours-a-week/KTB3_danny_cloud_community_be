55ed15dcb4f8fd2ae7dd8dd40ce535064448280d
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2025-10-12 22:03:32 +0900

    게시글 상세조회 기능 추가

--- src/main/java/com/ktb/community/controller/PostController.java
+++ src/main/java/com/ktb/community/controller/PostController.java
@@ -2,14 +2,14 @@ package com.ktb.community.controller;
 
 import com.ktb.community.dto.response.ApiResponseDto;
 import com.ktb.community.dto.response.CursorPageResponseDto;
+import com.ktb.community.dto.response.PostDetailResponseDto;
 import com.ktb.community.dto.response.PostResponseDto;
+import com.ktb.community.entity.Post;
 import com.ktb.community.service.PostService;
-import org.apache.coyote.Response;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,13 +23,13 @@ public class PostController {
         this.postService = postService;
     }
 
-    @GetMapping
+    @GetMapping()
     public ResponseEntity<ApiResponseDto<CursorPageResponseDto<PostResponseDto>>> getPosts(
             @RequestParam(required = false) Long cursor,
             @RequestParam(defaultValue = "20") int size
     ) {
         try {
-            CursorPageResponseDto<PostResponseDto> result = postService.getPosts(cursor, size);
+            CursorPageResponseDto<PostResponseDto> result = postService.getPostList(cursor, size);
             return ResponseEntity.ok(ApiResponseDto.success(result));
 
 
@@ -38,4 +38,22 @@ public class PostController {
                     .body(ApiResponseDto.error("Internal server error occured"));
         }
     }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<ApiResponseDto<PostDetailResponseDto>> getPostDetail(@PathVariable @Positive Long postId) {
+
+        try {
+            PostDetailResponseDto post = this.postService.getPostContent(postId);
+            return ResponseEntity.ok().body(ApiResponseDto.success(post));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(404)
+                    .body(ApiResponseDto.error(e.getMessage()));
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError()
+                    .body(ApiResponseDto.error("Internal server error occured"));
+        }
+
+
+    }
+
 }
--- src/main/java/com/ktb/community/service/PostService.java
+++ src/main/java/com/ktb/community/service/PostService.java
@@ -1,10 +1,13 @@
 package com.ktb.community.service;
 
 import com.ktb.community.dto.response.CursorPageResponseDto;
+import com.ktb.community.dto.response.PostDetailResponseDto;
 import com.ktb.community.dto.response.PostResponseDto;
 import com.ktb.community.entity.Count;
+import com.ktb.community.entity.Image;
 import com.ktb.community.entity.Post;
 import com.ktb.community.repository.CountRepository;
+import com.ktb.community.repository.ImageRepository;
 import com.ktb.community.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
@@ -18,14 +21,16 @@ import java.util.stream.Collectors;
 public class PostService {
     private final PostRepository postRepository;
     private final CountRepository countRepository;
+    private final ImageRepository imageRepository;
 
     @Autowired
-    public PostService(PostRepository postRepository, CountRepository countRepository) {
+    public PostService(PostRepository postRepository, CountRepository countRepository, ImageRepository imageRepository) {
         this.postRepository = postRepository;
         this.countRepository = countRepository;
+        this.imageRepository = imageRepository;
     }
 
-    public CursorPageResponseDto<PostResponseDto> getPosts(Long cursor, int size) {
+    public CursorPageResponseDto<PostResponseDto> getPostList(Long cursor, int size) {
         Pageable pageable = PageRequest.of(0, size + 1);
 
         List<Post> posts;
@@ -69,4 +74,39 @@ public class PostService {
         return new CursorPageResponseDto<>(postContent, nextCursor, hasNext);
     }
 
+    public PostDetailResponseDto getPostContent(Long postId) {
+        Post post = this.postRepository.findById(postId).orElse(null);
+
+        if (post == null) {
+            throw new IllegalArgumentException("The post does not exist.");
+        }
+
+        PostDetailResponseDto postDetailResponseDto = new PostDetailResponseDto();
+        postDetailResponseDto.setId(post.getId());
+        postDetailResponseDto.setTitle(post.getTitle());
+        postDetailResponseDto.setContent(post.getContent());
+        postDetailResponseDto.setAuthor(post.getUser().getNickname());
+
+        List<String> images = this.imageRepository.findByPostIdAndDeletedAtIsNull(post.getId())
+                .stream()
+                .map(image -> {
+                    return image.getUrl();
+                }).toList();
+        postDetailResponseDto.setImages(images);
+
+        Count count = this.countRepository.findByPostId(post.getId()).orElse(null);
+        if (count != null) {
+            postDetailResponseDto.setViews(count.getViewCount());
+            postDetailResponseDto.setLikes(count.getLikeCount());
+            postDetailResponseDto.setComments(count.getCommentCount());
+        } else {
+            postDetailResponseDto.setViews(0L);
+            postDetailResponseDto.setLikes(0L);
+            postDetailResponseDto.setComments(0L);
+        }
+
+        return postDetailResponseDto;
+    }
+
 }
+
