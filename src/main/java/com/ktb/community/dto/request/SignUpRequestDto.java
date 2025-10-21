ee88218c90808b44698bf481d81b8c976f10142f
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2025-10-21 15:04:37 +0900

    회원가입 테스트 추가

--- src/main/java/com/ktb/community/dto/request/SignUpRequestDto.java
+++ src/main/java/com/ktb/community/dto/request/SignUpRequestDto.java
@@ -4,12 +4,10 @@ import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignUpRequestDto {
